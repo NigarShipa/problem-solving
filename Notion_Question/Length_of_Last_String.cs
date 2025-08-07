757c9395f807c6f0596c33faca172bc03f1e7b6f
Nigar Sultana <74977812+NigarShipa@users.noreply.github.com>
2025-08-07 14:29:58 +0800

    Add Length_of_Last_String.cs

---
+++ Notion_Question/Length_of_Last_String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Notion_Question
+{
+    public class Length_of_Last_String
+    {
+        public static void LengthofLastString()
+            {
+            string sentence = "Hello World ";
+            int count = 0 ;
+            for(int i= sentence.Length-1; i>=0 ; i--)
+            {
+                if(sentence[i] == ' ' && count >0)
+                {
+                  break; 
+                }
+                if(sentence[i] != ' ')
+                {
+                    count++;
+                }
+            }
+              Console.WriteLine("Length of the last string is: " + count);
+            
+            }
+
+    }
+}
